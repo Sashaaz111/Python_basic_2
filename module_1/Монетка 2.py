134158f2197d1d08503534689e8bdaf8bce9e37b
Sasha <2209sashaz@gmail.com>
2022-12-11 20:57:31 +0300

    Done task 6

---
+++ module_1/Монетка 2.py
@@ -0,0 +1,11 @@
+def find_monetka(c1, c2, r):
+    if c1 <= r and c2 <= r:
+        print('Монетка где-то рядом')
+    else:
+        print('Монетки в области нет')
+
+print('Введите координаты монетки:')
+x=float(input("Введите x: "))
+y=float(input("Введите y: "))
+r=int(input("Введите радиус: "))
+find_monetka(x,y,r)
